8fcca84b11d3eae08cd22a52a67784fd9fc0528e
Your Name <santoshkkathar@gmail.com>
2022-04-23 02:30:24 +0530

    test 3commit

---
+++ src/test/java/com/Test/Test_3.java
@@ -0,0 +1,14 @@
+package com.Test;
+
+import org.testng.annotations.Test;
+
+import com.utility.BaseClass;
+
+public class Test_3 extends BaseClass {
+	
+	@Test
+	public void test3() {
+		System.out.println("Teast3");
+	}
+
+}
